122343b9576f34d1ca2e5001a9ca487c632a9054
Jacob Russell <geekbombergamez@gmail.com>
2016-04-17 16:28:46 +0100

    It's Alive!!!!!
    the button now opens and closes :smile_cat:

---
+++ Work.js
@@ -0,0 +1,32 @@
+
+    function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'none')
+          e.style.display = 'block';
+       else
+          e.style.display = 'none';
+    }
+
+    function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
+
+    function toggle_background(id) {
+      var b = document.getElementById(id);
+      if (b.style.background-color == '#ffffff')
+          b.style.background-color = '#69675B';
+      else
+          b.style.background-color = '#ffffff';
+    }
+
+    function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
